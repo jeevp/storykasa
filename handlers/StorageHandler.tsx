97d78e117083b64b5962844358127c06a5c46ae1
Fernandes <felipecpfernandes@gmail.com>
2023-10-17 12:30:35 -0300

    STK-23: Remove Storage Controller from the server and use StorageHandler from the client instead

--- handlers/StorageHandler.tsx
+++ handlers/StorageHandler.tsx
@@ -4,33 +4,60 @@ import {
     AVATAR_FILE_EXTENSION,
     RECORD_BUCKET_NAME,
     RECORD_FILE_EXTENSION,
-    MPEG_FILE_EXTENSION
+    MPEG_FILE_EXTENSION, STK_ACCESS_TOKEN
 } from "@/config";
-import generateHeaders from "@/handlers/generateHeaders";
+import {v4} from "uuid";
+import generateSupabaseHeaders from "@/service/utils/generateSupabaseHeaders";
+
+const MAX_FILE_SIZE = 50 * 1024 * 1024; // 50MB in bytes
 
 export default class StorageHandler {
+
     static async uploadFile(formData: FormData) {
-        if (!formData) throw new Error('unable to get new avatar file')
+       try {
+           if (!formData) throw new Error('unable to get new avatar file')
 
-        const uploadDetailsString = formData.get("uploadDetails");
-        if (!uploadDetailsString) throw new Error('uploadDetails not found');
+           const uploadDetailsString = formData.get("uploadDetails");
+           if (!uploadDetailsString) throw new Error('uploadDetails not found');
 
-        const uploadDetails = JSON.parse(uploadDetailsString as string);
+           const uploadDetails = JSON.parse(uploadDetailsString as string);
 
-        const allowedBucketNames = [AVATAR_BUCKET_NAME, RECORD_BUCKET_NAME];
-        if (!allowedBucketNames.includes(uploadDetails.bucketName)) {
-            throw new Error("Bucket not allowed");
-        }
+           const allowedBucketNames = [AVATAR_BUCKET_NAME, RECORD_BUCKET_NAME];
+           if (!allowedBucketNames.includes(uploadDetails.bucketName)) {
+               throw new Error("Bucket not allowed");
+           }
 
-        const allowedExtensions = [AVATAR_FILE_EXTENSION, RECORD_FILE_EXTENSION, MPEG_FILE_EXTENSION];
-        if (!allowedExtensions.includes(uploadDetails.extension)) {
-            throw new Error("File extension not allowed");
-        }
+           const allowedExtensions = [AVATAR_FILE_EXTENSION, RECORD_FILE_EXTENSION, MPEG_FILE_EXTENSION];
+           if (!allowedExtensions.includes(uploadDetails.extension)) {
+               throw new Error("File extension not allowed");
+           }
 
-        const headers = generateHeaders();
-        const response = await axios.post("/api/storage/files", formData, headers)
+           const file = formData.get("file")
+           // Check if the file size exceeds the maximum allowed size
+           if (file?.size > MAX_FILE_SIZE) {
+               throw new Error("File size exceeds the maximum allowed size");
+           }
 
-        return response.data.publicUrl;
+           const uuid = v4();
+           const { bucketName, extension } = JSON.parse(formData.get("uploadDetails") as string);
+
+           // The URL to which you will send the file upload request
+           const url = `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/${bucketName}/${uuid}.${extension}`;
+
+           // Now formData already contains the file, so no need to create a new FormData object or Blob
+
+           // Sending the upload request using axios
+           const response = await axios.post(url, formData, {
+               headers: generateSupabaseHeaders(localStorage.getItem(STK_ACCESS_TOKEN), "multipart/form-data")
+           });
+
+           if (response.status !== 200) {
+               throw new Error("File upload failed");
+           }
+
+           return `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${bucketName}/${uuid}.${extension}`;
+       } catch (error) {
+           throw new Error("Something went wrong")
+       }
     }
-
 }
