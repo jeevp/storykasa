595083185dcfd4aae6943aa6c7babd9a5efd815b
Jeev Prayaga <findjeev@gmail.com>
2023-10-24 23:11:24 -0700

    testing cookies across subdomains

--- HOC/withAuth.tsx
+++ HOC/withAuth.tsx
@@ -1,55 +1,64 @@
 import { useRouter } from 'next/router'
 import { useContext, useEffect, useState } from 'react'
 import AuthContext from '@/contexts/AuthContext'
-import decodeJWT from "@/utils/decodeJWT"
-import jwtDecode from 'jwt-decode'  // Import jwt-decode to validate tokens
-import { STK_ACCESS_TOKEN, STK_REFRESH_TOKEN } from "@/config"
-import AuthHandler from "@/handlers/AuthHandler";
+import decodeJWT from '@/utils/decodeJWT'
+import jwtDecode from 'jwt-decode' // Import jwt-decode to validate tokens
+import { STK_ACCESS_TOKEN, STK_REFRESH_TOKEN } from '@/config'
+import AuthHandler from '@/handlers/AuthHandler'
 
 const withAuth = (WrappedComponent: any) => {
-    return (props: any) => {
-        const { setCurrentUser } = useContext(AuthContext)
-        const router = useRouter()
-        const [hasToken, setHasToken] = useState(false);
+  return (props: any) => {
+    const { setCurrentUser } = useContext(AuthContext)
+    const router = useRouter()
+    const [hasToken, setHasToken] = useState(false)
 
-        const isTokenExpired = (token: string) => {
-            const decodedToken = jwtDecode(token);
-            // @ts-ignore
-            const expirationTime = decodedToken.exp * 1000;  // Convert to milliseconds
-            return expirationTime <= Date.now();
-        }
-
-        const handleLogin = (accessToken: string) => {
-            const user = decodeJWT(accessToken)
-            setCurrentUser(user)
-            setHasToken(true);
-        }
-
-        const handleSignOut = async () => {
-            await AuthHandler.signOut()
-            await router.push("/login")
-        }
-
-        useEffect(() => {
-            if (typeof window !== 'undefined') {
-                const accessToken = localStorage.getItem(STK_ACCESS_TOKEN);
-                const refreshToken = localStorage.getItem(STK_REFRESH_TOKEN);
-
-                if ((accessToken && isTokenExpired(accessToken) && refreshToken)
-                    || (!accessToken || isTokenExpired(accessToken))
-                ) {
-                    handleSignOut()
-                    return;
-                }
-
-                handleLogin(accessToken);
-            }
-        }, []);
-
-        if (!hasToken) return null;
-
-        return <WrappedComponent {...props} />
+    const isTokenExpired = (token: string) => {
+      const decodedToken = jwtDecode(token)
+      // @ts-ignore
+      const expirationTime = decodedToken.exp * 1000 // Convert to milliseconds
+      return expirationTime <= Date.now()
     }
+
+    const handleLogin = (accessToken: string) => {
+      const user = decodeJWT(accessToken)
+      setCurrentUser(user)
+      console.log(user)
+      setHasToken(true)
+      document.cookie = `loggedIn=true;domain=.storykasa.com;path=/`
+    }
+
+    const handleSignOut = async () => {
+      await AuthHandler.signOut()
+      await router.push('/login')
+    }
+
+    useEffect(() => {
+      if (typeof window !== 'undefined') {
+        const accessToken = localStorage.getItem(STK_ACCESS_TOKEN)
+        const refreshToken = localStorage.getItem(STK_REFRESH_TOKEN)
+
+        if (
+          (accessToken && isTokenExpired(accessToken) && refreshToken) ||
+          !accessToken ||
+          isTokenExpired(accessToken)
+        ) {
+          handleSignOut()
+          document.cookie = `loggedIn=false;domain=.storykasa.com;path=/`
+          return
+        }
+
+        handleLogin(accessToken)
+      }
+    }, [])
+
+    if (!hasToken) return null
+
+    return (
+      <>
+        <WrappedComponent {...props} />
+      </>
+    )
+  }
 }
 
-export default withAuth;
+export default withAuth
