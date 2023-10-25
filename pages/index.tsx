4ae4e9d06518947bd42ecce4df1910af67c7d2d3
Jeev Prayaga <findjeev@gmail.com>
2023-10-25 00:53:12 -0700

    redirect to storykasa.com if withoutAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,43 @@
-import React, {useEffect} from "react";
-import STKButton from "@/components/STKButton/STKButton";
-import {useRouter} from "next/navigation";
-import axios from "axios";
-import withoutAuth from "@/HOC/withoutAuth";
-import PageWrapper from "@/composedComponents/PageWrapper";
-
+'use client'
+import React, { useEffect } from 'react'
+// import STKButton from '@/components/STKButton/STKButton'
+import { useRouter } from 'next/navigation'
+// import axios from 'axios'
+import withoutAuth from '@/HOC/withoutAuth'
+// import PageWrapper from '@/composedComponents/PageWrapper'
 
 function Welcome() {
-    const router = useRouter()
+  const router = useRouter()
 
-    const goTo = async (route: string) => {
-        await router.push(route)
-    }
+  const goTo = async (route: string) => {
+    await router.push(route)
+  }
 
+  useEffect(() => {
+    document.cookie = `loggedIn=false;domain=.storykasa.com;path=/`
+    router.push('https://storykasa.com')
+  }, [])
 
-    return (
-        <PageWrapper>
-            <h1 className="m-0">Welcome to StoryKasa</h1>
-            <div className="mt-4">
-                <h2 className="m-0 text-2xl font-normal">
-                    StoryKasa is a platform where you can listen to stories or create your
-                    own!
-                </h2>
-            </div>
-            <div className="flex items-center mt-14">
-                <STKButton onClick={() => goTo('/signup')}>
-                    Create an account
-                </STKButton>
-                <div className="ml-4">
-                    <STKButton variant="outlined" onClick={() => goTo("/login")}>
-                        Log in
-                    </STKButton>
-                </div>
-            </div>
-        </PageWrapper>
-    )
+  return (
+    <></>
+    // <PageWrapper>
+    //   <h1 className="m-0">Welcome to StoryKasa</h1>
+    //   <div className="mt-4">
+    //     <h2 className="m-0 text-2xl font-normal">
+    //       StoryKasa is a platform where you can listen to stories or create your
+    //       own!
+    //     </h2>
+    //   </div>
+    //   <div className="flex items-center mt-14">
+    //     <STKButton onClick={() => goTo('/signup')}>Create an account</STKButton>
+    //     <div className="ml-4">
+    //       <STKButton variant="outlined" onClick={() => goTo('/login')}>
+    //         Log in
+    //       </STKButton>
+    //     </div>
+    //   </div>
+    // </PageWrapper>
+  )
 }
 
 export default withoutAuth(Welcome)
