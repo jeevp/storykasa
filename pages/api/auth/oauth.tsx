4b593f223905d2dc26375f7ed358ef56891e5e73
Fernandes <felipecpfernandes@gmail.com>
2023-10-26 16:14:45 -0300

    debug google auth

--- pages/api/auth/oauth.tsx
+++ pages/api/auth/oauth.tsx
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
-
+import supabaseService from "@/service/supabase";
+//
 const processOauth = async (req: NextRequest, res: NextResponse) => {
     try {
         // @ts-ignore
@@ -9,9 +10,20 @@ const processOauth = async (req: NextRequest, res: NextResponse) => {
         if (!code) return res.status(400).send({ message: "Code is missing." })
         // @ts-ignore
         const supabase = createPagesServerClient({ req, res })
-        const response = await supabase.auth.exchangeCodeForSession(String(code))
+        const { data, error } = await supabase.auth.exchangeCodeForSession(String(code))
+
+        if (error) {
+            // @ts-ignore
+            return res.status(400).send({ message: "Something went wrong" })
+        }
         // @ts-ignore
-        return res.status(201).send(response)
+        await supabaseService.auth.setSession({
+            refresh_token: data.session.refresh_token,
+            access_token: data.session.access_token
+        })
+
+        // @ts-ignore
+        return res.status(200).send(data)
 
     } catch (error) {
         console.error(error)
