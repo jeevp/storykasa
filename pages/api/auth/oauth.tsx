0adede1e8ec6dd8685d1a78597ce8b08d22b97d6
Fernandes <felipecpfernandes@gmail.com>
2023-10-26 15:31:54 -0300

    Remove deprecated imports

--- pages/api/auth/oauth.tsx
+++ pages/api/auth/oauth.tsx
@@ -3,16 +3,19 @@ import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
 
 const processOauth = async (req: NextRequest, res: NextResponse) => {
     try {
+        // @ts-ignore
         const { code } = req.body
-
+        // @ts-ignore
         if (!code) return res.status(400).send({ message: "Code is missing." })
-
+        // @ts-ignore
         const supabase = createPagesServerClient({ req, res })
         const response = await supabase.auth.exchangeCodeForSession(String(code))
+        // @ts-ignore
         return res.status(201).send({ message: "Authentication succeed." , response })
 
     } catch (error) {
         console.error(error)
+        // @ts-ignore
         return res.status(400).send({ message: "Something went wrong." })
     }
 }
