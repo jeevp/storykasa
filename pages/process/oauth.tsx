aa37623cc2a01a639499ae75d1016d04cc563402
Fernandes <felipecpfernandes@gmail.com>
2023-10-26 16:45:56 -0300

    Improve process oauth page layout

--- pages/process/oauth.tsx
+++ pages/process/oauth.tsx
@@ -3,6 +3,7 @@ import queryString from 'query-string';
 import STKLoading from "@/components/STKLoading/STKLoading";
 import {useRouter} from "next/router";
 import AuthHandler from "@/handlers/AuthHandler";
+import Image from "next/image";
 
 function OauthProcess() {
     const router = useRouter()
@@ -25,8 +26,18 @@ function OauthProcess() {
     }
 
     return (
-        <div className="flex items-center justify-center">
-            <STKLoading />
+        <div className="flex items-center flex-col justify-center" style={{ height: "100vh" }}>
+            <Image
+                src="/logo.svg"
+                width={0}
+                className="cursor-pointer h-auto"
+                height={0}
+                style={{ width: 280 }}
+                alt="StoryKasa logo"
+            />
+            <div className="mt-10">
+                <STKLoading />
+            </div>
         </div>
     )
 }
