6779911adaa938408d5b9cd2f1a0b0734a598670
Fernandes <felipecpfernandes@gmail.com>
2023-11-08 15:41:22 -0300

    hotfix: language is not showing on the story card

--- composedComponents/StoryCard/StoryCard.tsx
+++ composedComponents/StoryCard/StoryCard.tsx
@@ -11,53 +11,79 @@ export default function StoryCard({ story }: {
 
     return (
         <STKCard>
-            <div className="flex p-4">
-                <div className="flex justify-items-start">
-                    <STKAvatar src={story?.profileAvatar!} name={story?.profileName} />
+            <div className="p-4">
+                <div className="flex">
+                    <div className="flex justify-items-start">
+                        <STKAvatar src={story?.profileAvatar!} name={story?.profileName} />
+                    </div>
+                    <div className="w-full cursor-pointer ml-4">
+                        <div className="flex items-center justify-between w-full">
+                            {story?.profileName && (
+                                <label>
+                                    {story?.profileName}
+                                </label>
+                            )}
+
+                            <label className="text-xs">{format(story?.lastUpdated)}</label>
+                        </div>
+
+                        <label className="font-semibold text-lg">
+                            {story?.title}
+                        </label>
+
+                        <div className="lg:flex hidden items-center flex-wrap opacity-60 mt-4 pr-14">
+                            {story?.duration && (
+                                <div className="flex items-center mr-4 mb-1 lg:mb-0">
+                                    <Timer size={14} weight="bold" />
+                                    <label className="ml-1">
+                                        {Math.ceil(story?.duration / 60)} min
+                                    </label>
+                                </div>
+                            )}
+                            {story?.ageGroups && (
+                                <div className="flex items-center mr-4 mb-1 lg:mb-0">
+                                    <Baby size={14} weight="bold" />
+                                    <label className="ml-1">
+                                        {story?.ageGroupsShortLabel}
+                                    </label>
+                                </div>
+                            )}
+                            {story?.language && (
+                                <div className="flex items-center">
+                                    <GlobeSimple size={14} weight="bold" />
+                                    <label className="ml-1">
+                                        {story?.language}
+                                    </label>
+                                </div>
+                            )}
+                        </div>
+                    </div>
                 </div>
-                <div className="w-full cursor-pointer ml-4">
-                    <div className="flex items-center justify-between w-full">
-                        {story?.profileName && (
-                            <label>
-                                {story?.profileName}
+                <div className="flex lg:hidden items-center flex-wrap opacity-60 mt-4 pr-14">
+                    {story?.duration && (
+                        <div className="flex items-center mr-4 mb-1 lg:mb-0">
+                            <Timer size={14} weight="bold" />
+                            <label className="ml-1">
+                                {Math.ceil(story?.duration / 60)} min
                             </label>
-                        )}
-
-                        <label className="text-xs">{format(story?.lastUpdated)}</label>
-                    </div>
-
-                    <label className="font-semibold text-lg">
-                        {story?.title}
-                    </label>
-
-                    <div className="flex items-center flex-wrap opacity-60 mt-4 pr-14">
-                        {story?.duration && (
-                            <div className="flex items-center mr-4 mb-1 lg:mb-0">
-                                <Timer size={14} weight="bold" />
-                                <label className="ml-1">
-                                    {Math.ceil(story?.duration / 60)} min
-                                </label>
-                            </div>
-                        )}
-                        {story?.ageGroups && (
-                            <div className="flex items-center mr-4 mb-1 lg:mb-0">
-                                <Baby size={14} weight="bold" />
-                                <label className="ml-1">
-                                    {story?.ageGroupsShortLabel}
-                                </label>
-                            </div>
-                        )}
-                        {story?.language && (
-                            <div className="flex items-center">
-                                <GlobeSimple size={14} weight="bold" />
-                                <label className="ml-1">
-                                    {languages
-                                        .find((l) => l.name === story?.language!)
-                                        ?.code.toLocaleUpperCase()}
-                                </label>
-                            </div>
-                        )}
-                    </div>
+                        </div>
+                    )}
+                    {story?.ageGroups && (
+                        <div className="flex items-center mr-4 mb-1 lg:mb-0">
+                            <Baby size={14} weight="bold" />
+                            <label className="ml-1">
+                                {story?.ageGroupsShortLabel}
+                            </label>
+                        </div>
+                    )}
+                    {story?.language && (
+                        <div className="flex items-center">
+                            <GlobeSimple size={14} weight="bold" />
+                            <label className="ml-1">
+                                {story?.language}
+                            </label>
+                        </div>
+                    )}
                 </div>
             </div>
         </STKCard>
