940a9de450fe9060a6f11ec54b40870a0d48da90
Fernandes <felipecpfernandes@gmail.com>
2023-11-22 19:49:53 -0300

    STK-13: fix

--- pages/faq/index.tsx
+++ pages/faq/index.tsx
@@ -7,11 +7,14 @@ import FAQContents from "@/contents/FAQ"
 function Index() {
     return (
         <PageWrapper path="faq">
-            <div>
+            <div className="pb-10">
                 <h2 className="m-0 text-2xl">
                     FAQ
                 </h2>
-                <div className="mt-10 pb-10">
+                <div className="mt-4">
+                    <p className="text-base">Explore answers to common questions about StoryKasa and enhance your storytelling journey with us.</p>
+                </div>
+                <div className="mt-10">
                     {FAQContents.map((FAQContent, index) => (
                         <div key={index} className="first:mt-0 mt-2">
                             <STKAccordion
@@ -20,6 +23,13 @@ function Index() {
                             list={FAQContent.list} />
                         </div>
                     ))}
+                </div>
+                <div className="mt-10">
+                    <p className="m-0">
+                        If you have any other questions that were not addressed here, please feel free to email us
+                        at <a href="mailto:help@storykasa.com">help@storykasa.com</a> and we will try to get back to you within 24 hours.
+                    </p>
+
                 </div>
             </div>
         </PageWrapper>
