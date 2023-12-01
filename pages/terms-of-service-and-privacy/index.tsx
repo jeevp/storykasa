79bd6cd8cc9946d227c98e38b9efd3c4fed8cca7
Fernandes <felipecpfernandes@gmail.com>
2023-11-30 21:34:31 -0300

    STK-56: create terms of service and privacy page

---
+++ pages/terms-of-service-and-privacy/index.tsx
@@ -0,0 +1,261 @@
+import PageWrapper from "../../composedComponents/PageWrapper";
+
+export default function TermsOfServiceAndPrivacy() {
+    return (
+        <PageWrapper path="/terms-of-service-and-privacy">
+            <div className="terms-container pb-40">
+                <h1>Terms of Use and Privacy</h1>
+                <p className="effective-date">Effective date: December 1, 2023</p>
+
+                <section className="mt-6">
+                    <p>
+                        Please take the time to read and understand this Agreement before creating an account to
+                        access StoryKasa’s platform, website, and app (“Services”). By signing up you accept that
+                        you are entering into a contract with us on the Terms of Use (“Terms”) of this Agreement. Any
+                        visitors who access StoryKasa Services similarly affirm that they are bound by this Agreement
+                        even if they are not account holders.
+                    </p>
+                    <p className="mt-4">If you're agreeing to these Terms on behalf of an organization or entity (for example, if
+                        you’re an administrator agreeing to these Terms on behalf of your
+                        school/organization/district), you represent and warrant that you are authorized to agree
+                        to these Terms on that organization or entity's behalf and bind them to these Terms.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Description of Services</h2>
+                    <p>StoryKasa Services allow you to listen to audio stories or create audio stories on any
+                        device. The Discover feature helps you to browse the StoryKasa public library, listen to
+                        stories and save your favorites to your private library. The Create a Story feature lets
+                        you create, save, and share your own stories in your private library. When you record a new
+                        story you can add a title, description, age range, and upload illustrations. Your stories
+                        will be visible to other profiles in your account. You can also choose to share your stories
+                        with other friends and family using a link to a shared library. If you would like to share a
+                        story with the public you can submit your story for review and we may accept it based on
+                        certain assurances (e.g., is original or adapted and not under copyright) and
+                        criteria (e.g., has good sound quality). If you do this and we approve your story it
+                        will be converted to public and viewable by others. If you change your mind and would
+                        like to convert your story back to private, please email us at help@storykasa.com and we
+                        can help you with this.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Free Tier</h2>
+                    <p>
+                        StoryKasa provides a free tier of Services that is accessible to all and is designed to
+                        reach as many children, families, and schools as possible without cost-based barriers. We
+                        hope that stories and words can help language development and literacy, fuel
+                        imagination, and build empathy and social connectedness.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Paid Tier</h2>
+                    <p>
+                        We also have a premium paid tier of Services for families and schools who would like
+                        additional features and services such as support for larger numbers of users, personalized
+                        AI tools, and recording capabilities that exceed the one-hour limit in the free tier. Payments
+                        in the premium tier can be made on a monthly or annual basis, are non-refundable, and will
+                        be charged to your payment method on the billing date indicated. The length of your billing
+                        cycle will depend on the type of subscription that you choose when you sign-up for the
+                        service. If a payment is not successfully settled, due to expiration, insufficient funds, or
+                        otherwise, and you do not cancel your account, we may suspend your access to Services until
+                        we have successfully charged a valid payment method. You can cancel your premium membership
+                        at any time, and you will continue to have access to StoryKasa Services through the end of
+                        your billing period. We may change our subscription plans and the price of our Services from
+                        time to time; however, any price changes or changes to your subscription plans will apply at
+                        least 30 days after we notify you.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Creating an Account</h2>
+                    <p>
+                        Accounts may only be established by individuals 13 and older and by creating an account
+                        you affirm that you are at least 13 years old. Children below the age of 13 must have the
+                        account holder’s permission to listen to stories or create new stories using StoryKasa’s
+                        Services. An account holder can add two other people for a total of 3 profiles per account.
+                        Your email address must be kept up-to-date, and we may send important messages from time to
+                        time to your email address. You are solely responsible for the activity that occurs on your
+                        account, so it’s important that you keep your account secure and use a strong password. If
+                        you think that someone has gained access to your account, please reach out to us at
+                        help@storykasa.com
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Copyright and Creative Commons</h2>
+                    <p>
+                        Creative Commons (CC) is an international nonprofit organization dedicated to building
+                        capacity, developing practical solutions, and advocating for open sharing of knowledge and
+                        culture that serves the public interest and addresses the world’s most pressing challenges.
+                        By openly releasing all content in our public library under a Creative Commons license we are
+                        able to scale impact and make this content available to individuals and organizations
+                        worldwide. Much of our content is already in the public domain or openly licensed from
+                        others like StoryWeaver and Africa Storybook under a CC BY 4.0 license which lets you
+                        distribute, remix, and build upon the story as long as you provide attribution and credit
+                        the author and narrator.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Copyright and Creative Commons</h2>
+                    <p>
+                        Creative Commons (CC) is an international nonprofit organization dedicated to building
+                        capacity, developing practical solutions, and advocating for open sharing of knowledge and
+                        culture that serves the public interest and addresses the world’s most pressing
+                        challenges. By openly releasing all content in our public library under a Creative
+                        Commons license we are able to scale impact and make this content available to individuals
+                        and organizations worldwide. Much of our content is already in the public domain or openly
+                        licensed from others like StoryWeaver and Africa Storybook under a CC BY 4.0 license which
+                        lets you distribute, remix, and build upon the story as long as you provide attribution and
+                        credit the author and narrator.
+                    </p>
+                    <p className="mt-4">
+                        This also means that if you choose to share stories with the StoryKasa public library you
+                        are agreeing to making them available to anyone via a CC BY 4.0 license. Of course, you
+                        can always keep all your stories in your private library, and they will only be viewable
+                        by others in your account or specific people you choose to share with outside your
+                        account. You are responsible for following the law, especially the law as it relates to
+                        copyright infringement, while using StoryKasa Services. We do not have an obligation to
+                        monitor user conduct on our Services and you are solely responsible for your conduct,
+                        the content you submit, and your interactions with others.
+                    </p>
+                    <p className="mt-4">
+                        You are fully responsible for the content you share with us. If you share the content
+                        publicly, you do so at your own risk and StoryKasa will not be responsible for any
+                        legal actions with regards to such content. You hereby represent and warrant that
+                        (1) you own the content or have the rights to share the content with us and to allow us
+                        to use it, and (2) the content will not infringe, misappropriate, or otherwise violate
+                        the intellectual property rights or other rights of a third party.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Our Commitment to Privacy</h2>
+                    <p>
+                        Our commitment is to you, and we understand the value of your privacy. We want you to
+                        feel comfortable sharing your stories through StoryKasa and we want to explain the steps
+                        we take to ensure your stories stay safe and private. This means no ads, no data
+                        sharing, and no third-party access to your data. We do not allow advertisers to create
+                        profiles for the purpose of targeting users for advertising and marketing. If you have
+                        any questions about privacy, don’t hesitate to contact us at help@storykasa.com.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>User Control</h2>
+                    <p>
+                        All user created stories are private by default and remain in your personal library. They
+                        are only viewable by people in your account or others you may invite to listen to
+                        them. However, you can submit a story to share with the larger StoryKasa
+                        community. We will not share your story with the public via the website or social
+                        networks unless you explicitly request us to do this.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Saving and Securing Stories</h2>
+                    <p>
+                        These are your stories, and we want to make sure that no matter what happens, you will
+                        always have access to them. All stories are stored in our database and all communication
+                        between your browser and StoryKasa uses the secure https:// protocol (indicated by
+                        the little padlock next to the internet address) for additional security.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Saving and Securing Stories</h2>
+                    <p>
+                        These are your stories, and we want to make sure that no matter what happens, you will
+                        always have access to them. All stories are stored in our database and all communication
+                        between your browser and StoryKasa uses the secure https:// protocol (indicated by
+                        the little padlock next to the internet address) for additional security.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Acceptable Use</h2>
+                    <p>
+                        You may use our Services only as permitted by law and you may not misuse our Services in
+                        any way. For example, you may not
+                    </p>
+                    <ul>
+                        <li>
+                            access or use our Services to abuse, defame, slander, harass or threaten anyone.
+                        </li>
+                        <li>
+                            share any materials with us or through the Services that are obscene, inappropriate
+                            or objectionable, such as pornographic materials or materials that are racist,
+                            malicious or violent.
+                        </li>
+                        <li>
+                            use the Services to transmit any viruses or similar destructive items
+                        </li>
+                        <li>
+                            interfere with our Services or a user’s ability to access them, such as by
+                            hacking them or accessing another user’s accoun
+                        </li>
+                    </ul>
+                    <p>
+                        We may suspend or stop providing our Services to you at any time, including if you
+                        do not comply with these Terms or any other policies or if we are investigating
+                        suspected misconduct.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Violation of Terms of Use</h2>
+                    <p>
+                        You will lose the right to use StoryKasa Services if you violate these Terms, including
+                        violating intellectual property, unfair competition, copyright, or any other laws. We
+                        also reserve the right to terminate your access to all or part of any of the
+                        services, for any reason, with or without notice.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Disclaimer and Release</h2>
+                    <p>
+                        StoryKasa cannot control and has no responsibility for the truth or accuracy of any
+                        material provided by its users. Even though users are prohibited from doing so, people
+                        may provide information, or otherwise behave, in a way that is unreliable, misleading, or even
+                        illegal. To the extent that the law permits, you release StoryKasa, its directors, contractors
+                        and employees from all liability (including in relation to disputes with other users) arising
+                        out of your use of StoryKasa Services.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Indemnification and Limitation of Liability</h2>
+                    <p>
+                        You agree to defend, indemnify and hold harmless StoryKasa from all damages, losses, expenses
+                        (including reasonable legal fees), claims, suits or actions related to your breach of these
+                        Terms or violation of applicable laws. The laws of California, USA, will apply to any
+                        disputes arising out of or relating to these Terms or the Services.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Changes To These Terms</h2>
+                    <p>
+                        StoryKasa reserves the right to revise these Terms at any time by posting an updated
+                        version to our website. You should visit this page periodically to review the most current
+                        terms, because they are binding on you.
+                    </p>
+                </section>
+
+                <section>
+                    <h2>Disputes</h2>
+                    <p>
+                        If you are unhappy with our Services, please reach out to us. We try our best to listen
+                        to your concerns and resolve things amicably. To contact StoryKasa, please email us at
+                        hello@storykasa.com and we will get back to you as soon as possible.
+                    </p>
+                </section>
+            </div>
+        </PageWrapper>
+    )
+}
+
