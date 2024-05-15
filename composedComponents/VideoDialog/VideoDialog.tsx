400ca980341fcc0d443a4c38396181f335df474e
Fernandes <jpcr1rodrigues@gmail.com>
2024-05-15 16:15:30 -0300

    STK-8: implementing video helpers

--- composedComponents/VideoDialog/VideoDialog.tsx
+++ composedComponents/VideoDialog/VideoDialog.tsx
@@ -15,13 +15,12 @@ export default function VideoDialog({ active, onClose = () => ({}) }: VideoDialo
 
   return (
     <STKDialog fullScreen={onMobile} active={active} onClose={() => onClose()}>
-      <div>
-        <ReactPlayer
-          url="https://api.storykasa.com/storage/v1/object/public/storykasa-videos/Getting Started.mp4?t=2024-05-14T15%3A21%3A20.055Z"
-          width="100%"
-          height="100%"
-        />
-      </div>
+      <ReactPlayer
+        url="https://api.storykasa.com/storage/v1/object/public/storykasa-videos/Getting Started.mp4?t=2024-05-14T15%3A21%3A20.055Z"
+        width="100%"
+        height="100%"
+        playing={true}
+      />
     </STKDialog>
   );
 }
