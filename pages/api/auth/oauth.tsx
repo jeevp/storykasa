7b81500e24639344d437a3205237a9ffbef7b6a7
Fernandes <felipecpfernandes@gmail.com>
2024-06-05 17:54:55 -0300

    fix

--- pages/api/auth/oauth.tsx
+++ pages/api/auth/oauth.tsx
@@ -3,6 +3,11 @@ import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
 import supabaseService from "@/service/supabase";
 // @ts-ignore
 import Profile from "@/service/models/Profile";
+import StripeService from "@/service/services/StripeService/StripeService"
+const StripeAccount = require("../../../service/models/StripeAccount")
+const Subscription = require("../../../service/models/Subscription")
+const AccountToolsUsage = require("../../../service/models/AccountToolsUsage")
+
 //
 const processOauth = async (req: NextRequest, res: NextResponse) => {
     try {
@@ -35,6 +40,30 @@ const processOauth = async (req: NextRequest, res: NextResponse) => {
             refresh_token: data.session.refresh_token,
             access_token: data.session.access_token
         })
+
+        // Let's create a free account as default
+        const stripeCustomer = await StripeService.customers.create({ email: data.session?.user?.email })
+
+        const subscription = await StripeService.subscriptions.create({
+            customerId: stripeCustomer?.id,
+            planId: process.env.NEXT_PUBLIC_STRIPE_FREE_PRICE_ID
+        })
+
+        const stripeAccount = await StripeAccount.create({
+            accountId: data.user.id,
+            stripeSubscriptionId: subscription.id,
+            stripeCustomerId: stripeCustomer.id
+        })
+
+        const { FREE_SUBSCRIPTION_PLAN } = Subscription.getAllowedSubscriptionPlanNames()
+        await Subscription.create({
+            accountId: data.user.id,
+            stripeAccountId: stripeAccount.id,
+            subscriptionPlan: FREE_SUBSCRIPTION_PLAN,
+        })
+
+        // Let's create the account tools usage configuration
+        await AccountToolsUsage.create({ accountId: data.user.id })
 
         // @ts-ignore
         return res.status(200).send({
